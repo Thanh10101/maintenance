d8ae165863abe130dea7528bf783aadf1e91b684
Nguyễn Trịnh Thành <90306525+Thanh10101@users.noreply.github.com>
2024-05-08 13:05:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkAdmin(isAdmin) {
+    isAdmin = false;
+    return isAdmin;
+}
+
+function maintainpgage() {
+    if (checkAdmin()) {
+        console.log("admmin");
+    } else {
+        window.location.href = 'maintenance.html';
+    }
+}
+setTimeout("maintainpgage()", 5000);
